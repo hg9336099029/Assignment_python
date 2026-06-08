95071541eca3f6d4eda660d485e0dbc9be22b32e
Harsh_Gupta <hg9336099029@gmail.com>
2026-06-08 12:17:51 +0530

    handson

---
+++ 13_index_reverse-index+slicing/Handson/indexing+reverse+slicing.py
@@ -0,0 +1,26 @@
+# Indexing, Reverse Indexing, and Slicing in One Script
+
+s = "Python"
+
+print("String:", s)
+
+# Indexing
+print("s[0] =", s[0])   # P
+print("s[2] =", s[2])   # t
+
+# Reverse (Negative) Indexing
+
+print("s[-1] =", s[-1]) # n
+print("s[-3] =", s[-3]) # h
+
+# Slicing
+
+print("s[0:3] =", s[0:3])   # Pyt
+print("s[2:5] =", s[2:5])   # tho
+print("s[:4] =", s[:4])     # Pyth
+print("s[3:] =", s[3:])     # hon
+print("s[::2] =", s[::2])   # Pto
+
+# Reverse String
+
+print("s[::-1] =", s[::-1]) # nohtyP
